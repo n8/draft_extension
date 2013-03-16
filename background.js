b327e84707099ac4abce2e8ee6c74b7324175dc6
nate <nate@cityposh.com>
2013-03-15 20:30:02 -0500

    prod

--- background.js
+++ background.js
@@ -7,11 +7,11 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.sendMessage(tab.id, 'getCurrentValue', function(currentTargetValue){
       
 
-        chrome.cookies.set( {"domain": "127.0.0.1", "url": "http://127.0.0.1", "name": "currentTargetValue", "value": escape(currentTargetValue)}, function(currentValueCookie){
-          chrome.cookies.set( {"url": "http://127.0.0.1", "name": "currentTargetURL", "value": escape(target_tab.url)}, function(currentTargetURLCookie){
+        chrome.cookies.set( {"domain": "draftin.com", "url": "https://draftin.com", "name": "currentTargetValue", "value": escape(currentTargetValue)}, function(currentValueCookie){
+          chrome.cookies.set( {"url": "https://draftin.com", "name": "currentTargetURL", "value": escape(target_tab.url)}, function(currentTargetURLCookie){
 
   
-            chrome.tabs.create({url: "http://127.0.0.1:3000/documents/"}, function(draft_tab){
+            chrome.tabs.create({url: "https://draftin.com/documents/"}, function(draft_tab){
 
         
 
@@ -20,6 +20,22 @@ chrome.browserAction.onClicked.addListener(function(tab) {
           
           });
         });
+ 
+
+
+        // chrome.cookies.set( {"domain": "127.0.0.1", "url": "http://127.0.0.1", "name": "currentTargetValue", "value": escape(currentTargetValue)}, function(currentValueCookie){
+        //   chrome.cookies.set( {"url": "http://127.0.0.1", "name": "currentTargetURL", "value": escape(target_tab.url)}, function(currentTargetURLCookie){
+
+  
+        //     chrome.tabs.create({url: "http://127.0.0.1:3000/documents/"}, function(draft_tab){
+
+        
+
+        //       fetchDocumentContent(target_tab, draft_tab);
+        //     });
+          
+        //   });
+        // });
  
 
     });
